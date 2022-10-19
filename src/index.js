2205df20e9ead8af806892c0a3002b1a5f3ec84b
Daniel Anaya <malgron@hotmail.com>
2022-10-19 09:26:07 -0600

    Assessment 21.6

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Holiday from "./Holiday";
+
+ReactDOM.render(<App />, document.getElementById("root"));
